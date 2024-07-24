0a5b47d6c1487b3e1889a43deac25c143965ef7e
Denys Bokov <denisbokov1703@gmail.com>
2024-07-24 20:09:57 +0200

    feat(utils): create numberFixTo function

--- src/components/utils/functions/index.ts
+++ src/components/utils/functions/index.ts
@@ -5,3 +5,13 @@ export function isDislayCountFormatValid(format: string) {
   const regex = /^X{1,16}$/;
   return regex.test(format);
 }
+
+export function numberFixTo(number: number | string, format: string): string {
+  if (format && !isDislayCountFormatValid(format)) {
+    throw new Error(
+      'fixTo: fromat invalid. Please enter the correct format: X*16. Example: XXX.',
+    );
+  }
+
+  return Number(number).toFixed(format?.length);
+}
