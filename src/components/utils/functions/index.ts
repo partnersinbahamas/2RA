e4cb990e2303e739afedb4b2bdee81e7c0bce36c
Denys Bokov <denisbokov1703@gmail.com>
2024-07-24 20:09:44 +0200

    feat(utils): create isDislayCountFormatValid function

--- src/components/utils/functions/index.ts
+++ src/components/utils/functions/index.ts
@@ -1,2 +1,7 @@
 export const wait = (ms: number) =>
   new Promise(resolve => setTimeout(resolve, ms));
+
+export function isDislayCountFormatValid(format: string) {
+  const regex = /^X{1,16}$/;
+  return regex.test(format);
+}
