2010449fcf541e96f273c09753ecc75cfa31839a
Denys Bokov <denisbokov1703@gmail.com>
2024-08-30 17:50:50 +0200

    fix(release.config): update order with new @semantic-release/changelog

--- release.config.cjs
+++ release.config.cjs
@@ -3,11 +3,13 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
+    '@semantic-release/changelog',
+    '@semantic-release/npm',
     ["@semantic-release/git", {
       "assets": ["dist/*.js", "dist/*.js.map"],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
-    '@semantic-release/github'
+    '@semantic-release/github',
   ],
 };
   
