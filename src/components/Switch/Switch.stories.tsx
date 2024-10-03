6a7fa93a161f9b7258632b63f90d33e6611833ef
Denys Bokov <denisbokov1703@gmail.com>
2024-10-03 17:46:43 +0200

    feat(Switch): create meta story

---
+++ src/components/Switch/Switch.stories.tsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import { Meta, StoryObj } from '@storybook/react';
+import { Switch } from './Switch';
+import { story } from '../utils/variables/story';
+import { ArgTypes } from 'storybook/internal/types';
+import { DocsPage, Subtitle } from '@storybook/blocks';
+
+const meta = {
+  component: Switch,
+  title: 'Athomic/Switch',
+  tags: ['autodocs'],
+  args: {
+    label: 'Switch',
+  },
+  argTypes: {
+    onClick: story.onClick,
+    label: story.title,
+    defaultToggle: story.boolean,
+    required: story.boolean,
+  } as ArgTypes,
+  parameters: {
+    layout: 'centered',
+    docs: {
+      page: () => {
+        return (
+          <>
+            <Subtitle>
+              Please to style your custom Input component use the mockup bellow.
+              below.
+            </Subtitle>
+            {/* <Markdown>Guide styles | .module.scss</Markdown>
+            <Source language="css" code={INPUT_MODULE_SCSS_STYLES} />
+
+            <Markdown>Guide styles | .scss</Markdown>
+            <Source language="css" code={INPUT_SCSS_STYLES} /> */}
+            <DocsPage />
+          </>
+        );
+      },
+    },
+  },
+} satisfies Meta<typeof Switch>;
+export default meta;
+
+type TStory = StoryObj<typeof meta>;
+export const DefaultSwitch: TStory = {};
