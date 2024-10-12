96af7c1dc2583b86996ad6acc82e753511c486d4
Denys Bokov <denisbokov1703@gmail.com>
2024-10-12 17:34:26 +0200

    refactor(release.config): update changelog file and git assets

--- release.config.cjs
+++ release.config.cjs
@@ -3,10 +3,12 @@ const config = {
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    ["@semantic-release/changelog", {
+      "changelogFile": "CHANGELOG.md"
+    }],
     '@semantic-release/npm',
     ["@semantic-release/git", {
-      "assets": ["dist/*.js", "dist/*.js.map"],
+      "assets": ["package.json", "package-lock.json", "CHANGELOG.md", "dist/*.js", "dist/*.js.map",],
       "message": "chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}"
     }],
     '@semantic-release/github',
